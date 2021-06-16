00038edc019bfb4e397889afc02460031d3f507f
Swami R Kalagiri <swami.kalagiri@gmail.com>
2021-06-16 11:59:35 -0500

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.27"
-    }
-  }
-
-  required_version = ">= 0.14.9"
-}
-
 provider "aws" {
-  profile = "default"
-  region  = "us-west-1"
-}
+  region = "us-east-1"
 }
